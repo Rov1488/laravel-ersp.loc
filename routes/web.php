b5223b04112706633508f606e102646ee50aa22c
R.Pulatov <r.pulatov1488@gmail.com>
2025-02-14 18:07:59 +0500

    yangi ozgarishlar CRUD resource

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\PageController::class, 'main']);
@@ -16,6 +17,9 @@ Route::get('/posts/show/{post_id}/{title}', [PostController::class, 'show']);
 //Route::get('/posts/create', [PostController::class, 'create']);
 Route::addRoute(['GET', 'POST'], '/posts/create', [PostController::class, 'create']);
 Route::post('/posts/post-create', [PostController::class, 'store']);
+
+Route::resource('products', [ProductController::class]);
+
 
 
 
