6b2fb8629245eba3f3e88d7ed0e7a540a018483c
R.Pulatov <r.pulatov1488@gmail.com>
2025-03-07 11:36:06 +0500

    post categoriya post_tag va tags tablelar migrationlari

--- database/migrations/2025_03_06_112534_create_comments_table.php
+++ database/migrations/2025_03_06_112534_create_comments_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('content');
             $table->timestamps();
-
         });
     }
 
---
+++ database/migrations/2025_03_07_061927_create_posts_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->unsignedBigInteger('category_id');
+            $table->string('language');
+            $table->text('description');
+            $table->text('content');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('author_id');
+            $table->timestamps();
+            $table->foreign('author_id')->references('id')->on('users');
+
+
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
+        });
+
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('post_tag');
+        Schema::dropIfExists('tags');
+    }
+};
