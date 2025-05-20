9f95473dd52b1ad95aea77dcfbb240e724b42bfb
R.Pulatov <r.pulatov1488@gmail.com>
2025-05-20 18:07:18 +0500

    artisan stub file publishe

--- resources/views/export-file/export.blade.php
+++ resources/views/export-file/export.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid align-content-center border-4 bg-gradient-to-br">
     <h2>Export excel file </h2>
     <div class="w-6 backdrop-blur-2xl border">
-    @dump($posts)
+    {{--@dump($posts)--}}
 
     </div>
 
